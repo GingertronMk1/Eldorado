1dd9af89428b2f1147c1279c941eefbbe2dcd891
Jack <jackellis1504@gmail.com>
2019-07-13 16:39:07 +0100

    Remove colons

--- Laravel/resources/views/items/show.blade.php
+++ Laravel/resources/views/items/show.blade.php
@@ -5,15 +5,15 @@
 @section('content')
 <ul>
     <li>
-      <h2>Name:</h2>
+      <h2>Name</h2>
       {{ $inventory->name }}
     </li>
     <li>
-      <h2>Description:</h2>
+      <h2>Description</h2>
       {{ $inventory->description }}
     </li>
     <li>
-      <h2>Unit Cost Price:</h2>
+      <h2>Unit Cost Price</h2>
       {{ $inventory->unit_cost_price }}
     </li>
     </ul>
