39f3cff121f888314deaeedc231ddfec8d73b482
Jack <jackellis1504@gmail.com>
2019-07-13 18:46:47 +0100

    Forcing currency representation

--- Laravel/resources/views/items/show.blade.php
+++ Laravel/resources/views/items/show.blade.php
@@ -12,7 +12,7 @@
         </li>
         <li>
             <h3>Unit Cost Price</h3>
-            {{ $inventory->unit_cost_price }}
+            £ {{ number_format($inventory->unit_cost_price, 2, '.', ',') }}
         </li>
     </ul>
     <a href="/inventory/{{ $inventory->id }}/edit">Edit this item</a></li>
