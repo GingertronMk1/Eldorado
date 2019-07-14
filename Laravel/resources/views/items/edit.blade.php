a5009e238925143695469e1cfb6660cd74d273f2
Jack <jackellis1504@gmail.com>
2019-07-14 18:23:10 +0100

    Made a proper form template file

--- Laravel/resources/views/items/edit.blade.php
+++ Laravel/resources/views/items/edit.blade.php
@@ -1,56 +1,11 @@
-@extends('items.layout')
+@extends('items.form')
 
-@section('Title', 'Editor')
-
-@section('content')
-    <h1>Edit {{ $inventory->name }}</h1>
-    <div class="form-group">
-        <form method="POST" action="/inventory/{{ $inventory->id }}">
-            @method('PATCH')
-            @csrf
-
-            <label for="name">Item Name</label>
-            <br>
-            <input type="text"
-                   class="form-control form-control-lg {{ $errors->has('name') ? 'is-invalid' : '' }}"
-                   name="name"
-                   placeholder="Item Name"
-                   value="{{ $inventory->name }}">
-
-            <br>
-
-            <label for="description">Description</label>
-
-            <br>
-
-            <textarea class="form-control {{ $errors->has('description') ? 'is-invalid' : '' }}" name="description">{{ $inventory->description }}</textarea>
-
-            <br>
-
-            <label for="unit_cost_price">Unit Cost Price</label>
-
-            <br>
-
-            <input type="number"
-                   class="form-control {{ $errors->has('unit_cost_price') ? 'is-invalid' : '' }}"
-                   name="unit_cost_price"
-                   step="0.01"
-                   value="{{ number_format($inventory->unit_cost_price, 2, '.', ',') }}">
-            <br>
-
-            <button type="submit" class="btn btn-primary">Update Item</button>
-        </form>
-        <br>
-    </div>
-
-    @if($errors->any())
-        <div class="bg-danger rounded">
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li style="color:white">{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
+@section('form_action')
+    /inventory/{{ $inventory->id }}
 @endsection
+
+@section('form_method')
+    @method('PATCH')
+@endsection
+
+@section('button_text', 'Update Item')
--- Laravel/resources/views/items/form.blade.php
+++ Laravel/resources/views/items/form.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     @yield('before_form')
 
-    <form method="POST" action="/inventory">
-        @yield('form_front')
+    <form method="POST" action=@yield('form_action')>
+        @yield('form_method')
 
         @csrf
 
@@ -13,8 +13,7 @@
         <input type="text"
                class="form-control form-control-lg {{ $errors->has('name') ? 'is-invalid' : '' }}"
                name="name"
-               placeholder="Item Name"
-               value="{{ ( ! empty($inventory) ? $inventory->name : '' ) }}">
+               value="{{ ( ! empty($inventory) ? $inventory->name : old('name') ) }}">
 
         <br>
 
@@ -22,7 +21,7 @@
 
         <br>
 
-        <textarea class="form-control {{ $errors->has('description') ? 'is-invalid' : '' }}" name="description">{{ ( ! empty($inventory) ? $inventory->description : '' ) }}</textarea>
+        <textarea class="form-control {{ $errors->has('description') ? 'is-invalid' : '' }}" name="description">{{ ( ! empty($inventory) ? $inventory->description : old('description') ) }}</textarea>
 
         <br>
 
@@ -34,14 +33,14 @@
                class="form-control {{ $errors->has('unit_cost_price') ? 'is-invalid' : '' }}"
                name="unit_cost_price"
                step="0.01"
-               value="{{ ( ! empty($inventory) ? $inventory->unit_cost_price : '' ) }}">
+               value="{{ ( ! empty($inventory) ? $inventory->unit_cost_price : old('unit_cost_price') ) }}">
 
         <br>
 
-        @yield('button')
+        <button type="submit" class="btn btn-primary">
+            @yield('button_text')
+        </button>
     </form>
-
-    @yield('after_form')
 
         @if($errors->any())
             <div class="bg-danger rounded">
@@ -52,4 +51,7 @@
                 </ul>
             </div>
         @endif
+
+    @yield('after_form')
+
 @endsection
