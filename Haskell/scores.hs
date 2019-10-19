e3a26eb1403c94a2a4d4c633e564f7de2f7eca47
Jack Ellis <jackellis1504@gmail.com>
2019-10-19 12:27:07 +0100

    Additions

--- Haskell/scores.hs
+++ Haskell/scores.hs
@@ -1,38 +1,54 @@
-nfls :: Int -> [String]
-nfls n = [show td ++ " TD, " ++ show pat ++ " PAT, " ++ show tpa ++ " 2PA, " ++ show fg ++ " FG, " ++ show s ++ " S"
-          |td  <-[0..div n 6]
-          ,pat <-[0..td]
-          ,tpa <-[0..td-pat]
-          ,fg  <-[0..div n 3]
-          ,s   <-[0..div n 2]
-          ,((6*td)+pat+(3*fg)+(2*(s+tpa)))==n]
-
-rus :: Int -> [String]
-rus n = [show try ++ " T, " ++ show con ++ " Con, " ++ show thr ++ " Pen/DG"
-         |try <-[0..div n 5]
-         ,con <-[0..try]
-         ,thr <-[0..div n 3]
-         ,((5*try)+(2*con)+(3*thr))==n]
-
-rls :: Int -> [String]
-rls n = [show try ++ " T, " ++ show con ++ " Con, " ++ show pen ++ " Pen, " ++ show dg ++ " DG"
-         |try <-[0..div n 4]
-         ,con <-[0..try]
-         ,pen <-[0..div n 2]
-         ,dg  <-[0..n]
-         ,((4*try)+(2*(con+pen))+dg)==n]
-
-intersperse :: a -> [a] -> [a]
-intersperse _ []     = []
-intersperse _ (n:[]) = [n]
-intersperse a (n:ns) = n:a:(intersperse a ns)
-
 ppStrings :: [String] -> IO ()
 ppStrings = putStrLn . concat . intersperse "\n"
+            where intersperse _ []     = []
+                  intersperse _ [n]    = [n]
+                  intersperse a (n:ns) = n:a:(intersperse a ns)
 
 nfl :: Int -> IO ()
 nfl = ppStrings . nfls
+      where nfls n = [ show td
+                   ++ " TD,\t"  -- Touchdowns
+                   ++ show pat
+                   ++ " PAT,\t" -- Point Afters
+                   ++ show tpa
+                   ++ " 2PA,\t" -- 2-point tries
+                   ++ show fg
+                   ++ " FG,\t"  -- Field Goals
+                   ++ show s
+                   ++ " S,\t"   -- Safeties
+                    | td  <-[0..div n 6]
+                    , pat <-[0..td]
+                    , tpa <-[0..td-pat]
+                    , fg  <-[0..div n 3]
+                    , s   <-[0..div n 2]
+                    , (6 * td) + pat + (3 * fg) + (2 * (s + tpa)) == n ]
+
 ru :: Int -> IO ()
 ru = ppStrings . rus
+     where rus n = [ show try
+                  ++ " T,\t"    -- Tries
+                  ++ show con
+                  ++ " Con,\t"  -- Conversions
+                  ++ show thr
+                  ++ " Pen/DG"  -- Penalties/Drop Goals
+                   | try <-[0..div n 5]
+                   , con <-[0..try]
+                   , thr <-[0..div n 3]
+                   , (5 * try) + (2 * con) + (3 * thr) == n ]
+
+
 rl :: Int -> IO ()
 rl = ppStrings . rls
+     where rls n = [ show try
+                  ++ " T,\t"    -- Tries
+                  ++ show con
+                  ++ " Con,\t"  -- Conversions
+                  ++ show pen
+                  ++ " Pen,\t"  -- Penalty Goals
+                  ++ show dg
+                  ++ " DG"      -- Drop Goals
+                   | try <-[0..div n 4]
+                   , con <-[0..try]
+                   , pen <-[0..div n 2]
+                   , dg  <-[0..n]
+                   , (4 * try) + (2 * (con + pen)) + dg == n ]
